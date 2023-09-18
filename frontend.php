c98d5af91c464b02e619eced8895e1dab2ba5d10
rmens <r.mens@me.com>
2023-09-18 20:07:59 +0200

    Update frontend.php

--- frontend.php
+++ frontend.php
@@ -9,6 +9,5 @@ function add_manifest_link() {
     echo '<meta name="apple-touch-fullscreen" content="yes">';
     echo '<meta name="application-name" content="' . esc_attr(get_bloginfo('name')) . '">';
     echo '<meta name="theme-color" content="#1f2937">';
-
     echo '<link rel="manifest" href="' . esc_url(get_rest_url(null, '/webapp/manifest/')) . '">';
 }
