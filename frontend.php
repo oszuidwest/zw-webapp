435c66d871b7058fc94aab1a297705619dc5f1c9
rmens <r.mens@me.com>
2023-09-18 22:12:59 +0200

    Add a splash screen

--- frontend.php
+++ frontend.php
@@ -8,5 +8,6 @@ function add_manifest_link() {
     echo '<meta name="apple-touch-fullscreen" content="yes">';
     echo '<meta name="application-name" content="' . esc_attr(get_bloginfo('name')) . '">';
     echo '<meta name="theme-color" content="#1f2937">';
+    echo '<link rel="apple-touch-startup-image" href="' . plugins_url('img/splash-test.png', __FILE__) . '">';
     echo '<link rel="manifest" href="' . esc_url(get_rest_url(null, '/webapp/manifest/')) . '">';
 }
