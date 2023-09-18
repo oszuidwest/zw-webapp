281f41529a44c98aeb6729dd51a0edf5ce19b473
Raymon Mens <r.mens@me.com>
2023-09-18 21:54:20 +0200

    Test deply

--- zw-webapp.php
+++ zw-webapp.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ZuidWest Webapp
  * Description: Generates a manifest.json using wp-json API and uses the site icon set in the Customizer.
- * Version: 0.1
+ * Version: 0.2
  * Author: Raymon Mens
  */
 
