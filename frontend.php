ef8f1f31dae36e9284a363144351735e151df3e4
rmens <r.mens@me.com>
2023-09-19 18:24:21 +0200

    Add progressier

--- frontend.php
+++ frontend.php
@@ -3,6 +3,8 @@
 add_action('wp_head', 'add_manifest_link');
 
 function add_manifest_link() {
-    
+    // Add the manifest link tag
+    echo '<link rel="manifest" href="https://progressier.app/bgJfPKpFdg73WEIh0imN/progressier.json"/>' . "\n";
+    echo '<script defer src="https://progressier.app/bgJfPKpFdg73WEIh0imN/script.js"></script>' . "\n";
     echo '<meta name="theme-color" content="#1f2937"/>' . "\n";
 }
