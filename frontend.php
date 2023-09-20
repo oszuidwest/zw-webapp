5d0b90a09f358367e32f2ee8cd6143b247d5b5df
Raymon Mens <r.mens@me.com>
2023-09-20 15:14:29 +0200

    Update frontend.php

--- frontend.php
+++ frontend.php
@@ -4,7 +4,7 @@ add_action('wp_head', 'add_manifest_link');
 
 function add_manifest_link() {
     // Add the manifest link tag
-    echo '<link rel="manifest" href="https://progressier.app/bgJfPKpFdg73WEIh0imN/progressier.json"/>' . "\n";
-    echo '<script defer src="https://progressier.app/bgJfPKpFdg73WEIh0imN/script.js"></script>' . "\n";
+    echo '<link rel="manifest" href="https://progressier.app/m8bdFVT3WcoUOcqtAg3e/progressier.json"/>' . "\n";
+    echo '<script defer src="https://progressier.app/m8bdFVT3WcoUOcqtAg3e/script.js"></script>' . "\n";
     echo '<meta name="theme-color" content="#1f2937"/>' . "\n";
 }
