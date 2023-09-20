91b47b38c3df7e6c8d0842da8179b83900ac58d1
Raymon Mens <r.mens@me.com>
2023-09-20 15:16:36 +0200

    Deployment (#1)

--- frontend.php
+++ frontend.php
@@ -1,13 +1,10 @@
 <?php
-declare(strict_types=1);
 // Link to the Manifest in the theme
 add_action('wp_head', 'add_manifest_link');
 
 function add_manifest_link() {
-    
-    echo '<meta name="mobile-web-app-capable" content="yes">';
-    echo '<meta name="apple-touch-fullscreen" content="yes">';
-    echo '<meta name="application-name" content="' . esc_attr(get_bloginfo('name')) . '">';
-    echo '<meta name="theme-color" content="#1f2937">';
-    echo '<link rel="manifest" href="' . esc_url(get_rest_url(null, '/webapp/manifest/')) . '">';
+    // Add the manifest link tag
+    echo '<link rel="manifest" href="https://progressier.app/m8bdFVT3WcoUOcqtAg3e/progressier.json"/>' . "\n";
+    echo '<script defer src="https://progressier.app/m8bdFVT3WcoUOcqtAg3e/script.js"></script>' . "\n";
+    echo '<meta name="theme-color" content="#1f2937"/>' . "\n";
 }
--- zw-webapp.php
+++ zw-webapp.php
@@ -1,11 +1,9 @@
 <?php
 /**
  * Plugin Name: ZuidWest Webapp
- * Description: Generates a manifest.json using wp-json API and uses the site icon set in the Customizer.
- * Version: 0.2
+ * Description: Integrates Progressier PWA
+ * Version: 1.0
  * Author: Raymon Mens
  */
 
-require_once plugin_dir_path(__FILE__) . 'routes.php';
-require_once plugin_dir_path(__FILE__) . 'manifest.php';
 require_once plugin_dir_path(__FILE__) . 'frontend.php';
