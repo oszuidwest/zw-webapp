e929825764df9de647776022a551e87a9c2e69e2
rmens <r.mens@me.com>
2023-09-24 00:46:14 +0200

    Validate

--- options.php
+++ options.php
@@ -8,8 +8,8 @@ add_action('admin_menu', 'zw_webapp_add_admin_menu');
 
 // Register settings
 function zw_webapp_settings_init() {
-    register_setting('pluginPage', 'zw_webapp_settings');
-
+    register_setting('pluginPage', 'zw_webapp_settings', 'validate_zw_webapp_settings');
+    
     add_settings_section(
         'zw_webapp_pluginPage_section',
         __('ZuidWest Webapp Settings', 'wordpress'),
@@ -37,16 +37,19 @@ function zw_webapp_settings_init() {
 }
 add_action('admin_init', 'zw_webapp_settings_init');
 
+// Render settings
 function zw_webapp_settings_render($args) {
     $options = get_option('zw_webapp_settings');
     $field_value = isset($options[$args[0]]) ? esc_attr($options[$args[0]]) : '';
     echo "<input type='text' name='zw_webapp_settings[" . esc_attr($args[0]) . "]' value='" . $field_value . "'>";
 }
 
-function zw_webapp_settings_section_callback() {
-    echo __('Set the values required for the ZuidWest Webapp integration.', 'wordpress');
+// Callback for settings section (can be expanded if needed)
+function zw_webapp_settings_section_callback() { 
+    // This can contain any additional description or content for the settings section
 }
 
+// Options page rendering
 function zw_webapp_options_page() {
     ?>
     <form action='options.php' method='post'>
@@ -59,3 +62,31 @@ function zw_webapp_options_page() {
     </form>
     <?php
 }
+
+// Validation for the settings
+function validate_zw_webapp_settings($value) {
+    $old_value = get_option('zw_webapp_settings');
+    
+    // List of fields that should not be empty
+    $required_fields = ['theme_color', 'progressier_id', 'auth_token'];
+    
+    foreach ($required_fields as $field) {
+        if (empty($value[$field])) {
+            // Add an error message to be displayed in the admin
+            add_settings_error(
+                'zw_webapp_settings',
+                $field . '_error',
+                sprintf('Error: %s cannot be empty.', $field),
+                'error'
+            );
+            
+            // Return the old value to prevent the new empty value from being saved
+            return $old_value;
+        }
+    }
+    
+    // If all fields are valid, return the sanitized value
+    return $value;
+}
+
+?>
