95a12a71c47bc9e6cf77612e3a99fefd43741e13
rmens <r.mens@me.com>
2023-09-26 22:11:39 +0200

    Fix more

--- frontend.php
+++ frontend.php
@@ -5,16 +5,16 @@
 add_action('wp_head', 'add_manifest_link');
 
 function add_manifest_link() {
-    $webapp_settings = get_option("zw_webapp_settings");
+    $webapp_settings = get_option('zw_webapp_settings');
 
     // Check for required settings
-    if (empty($webapp_settings["progressier_id"]) || empty($webapp_settings["theme_color"])) {
+    if (empty($webapp_settings['progressier_id']) || empty($webapp_settings['theme_color'])) {
         return;
     }
 
-    $progressier_id = esc_attr($webapp_settings["progressier_id"]);
-    $theme_color = esc_attr($webapp_settings["theme_color"]);
-    $base_url = "https://progressier.app/{$progressier_id}";
+    $progressier_id = esc_attr($webapp_settings['progressier_id']);
+    $theme_color = esc_attr($webapp_settings['theme_color']);
+    $base_url = 'https://progressier.app/{$progressier_id}';
 
     // Output the tags
     echo '<link rel="manifest" href="' . esc_url("{$base_url}/progressier.json") . '"/>' . "\n";
