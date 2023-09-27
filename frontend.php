4b2e9cec064a5b6d304beb76e18aacbcb4c24c12
rmens <r.mens@me.com>
2023-09-27 19:23:44 +0200

    Update frontend.php

--- frontend.php
+++ frontend.php
@@ -19,10 +19,10 @@ function zw_webapp_add_manifest_link()
 
     // Output the tags
     echo '<link rel="manifest" href="' . esc_url($base_url . '/progressier.json') . '"/>' . "\n";
-add_action('wp_enqueue_scripts', function() {
-    global $webapp_settings;
-    $script_url = esc_url('https://progressier.app/' . $webapp_settings['progressier_id'] . '/script.js');
-    wp_enqueue_script('progressier_script', $script_url, array(), null, true);
-});
+    add_action('wp_enqueue_scripts', function () {
+        global $webapp_settings;
+        $script_url = esc_url('https://progressier.app/' . $webapp_settings['progressier_id'] . '/script.js');
+        wp_enqueue_script('progressier_script', $script_url, array(), null, true);
+    });
     echo '<meta name="theme-color" content="' . $theme_color . '"/>' . "\n";
 }
