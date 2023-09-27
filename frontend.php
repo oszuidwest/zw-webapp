123098a4b7e5cce45f0a7d710a3e6c21679d9fd2
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2023-09-27 21:44:09 +0200

    Fix frontend.php

--- frontend.php
+++ frontend.php
@@ -2,27 +2,31 @@
 /**
  * Add link to the Manifest in the theme.
  */
-add_action('wp_head', 'zw_webapp_add_manifest_link');
+add_action('wp_enqueue_scripts', 'zw_webapp_enqueue_scripts');
+add_action('wp_head', 'zw_webapp_head_tags');
 
-function zw_webapp_add_manifest_link()
+function zw_webapp_enqueue_scripts()
 {
     $webapp_settings = get_option('zw_webapp_settings');
 
-    // Check for required settings
-    if (empty($webapp_settings['progressier_id']) || empty($webapp_settings['theme_color'])) {
-        return;
+    if (!empty($webapp_settings['progressier_id'])) {
+        $base_url = 'https://progressier.app/' . $webapp_settings['progressier_id'];
+        wp_enqueue_script('progressier', $base_url . '/script.js', [], false, ['strategy' => 'defer']);
+    }
+}
+
+function zw_webapp_head_tags()
+{
+    $webapp_settings = get_option('zw_webapp_settings');
+
+    if (!empty($webapp_settings['progressier_id'])) {
+        $base_url = 'https://progressier.app/' . $webapp_settings['progressier_id'];
+        echo '<link rel="manifest" href="' . esc_url($base_url . '/progressier.json') . '"/>' . "\n";
     }
 
-    $progressier_id = esc_attr($webapp_settings['progressier_id']);
-    $theme_color = esc_attr($webapp_settings['theme_color']);
-    $base_url = 'https://progressier.app/{$progressier_id}';
-
-    // Output the tags
-    echo '<link rel="manifest" href="' . esc_url($base_url . '/progressier.json') . '"/>' . "\n";
-    add_action('wp_enqueue_scripts', function () {
-        global $webapp_settings;
-        $script_url = esc_url('https://progressier.app/' . $webapp_settings['progressier_id'] . '/script.js');
-        wp_enqueue_script('progressier_script', $script_url, array(), null, true);
-    });
-    echo '<meta name="theme-color" content="' . $theme_color . '"/>' . "\n";
+    // Check for required settings
+    if (!empty($webapp_settings['theme_color'])) {
+        $theme_color = esc_attr($webapp_settings['theme_color']);
+        echo '<meta name="theme-color" content="' . $theme_color . '"/>' . "\n";
+    }
 }
