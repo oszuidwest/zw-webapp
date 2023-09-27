7d606c681a721cce1ef593b053cc4834c685a204
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2023-09-27 23:25:17 +0200

    Remove streekomroep-specific code

--- push.php
+++ push.php
@@ -3,13 +3,6 @@
 add_action('save_post', 'zw_webapp_schedule_push_notification', 20, 3);
 add_action('send_push_notification', 'zw_webapp_call_api');
 add_action('edit_form_top', 'zw_webapp_show_debug_message', 10, 1);
-add_filter('zw_webapp_title', 'zw_webapp_push_title', 10, 2);
-add_filter('zw_webapp_send_notification', 'zw_webapp_send_notification', 10, 2);
-
-function zw_webapp_send_notification($do_send, $post_id)
-{
-    return get_field('push_post', $post_id);
-}
 
 function zw_webapp_schedule_push_notification($post_id, $post, $update)
 {
@@ -53,32 +46,6 @@ function zw_webapp_get_featured_image_url($post_id)
     }
 
     return $image_url;
-}
-
-function zw_webapp_push_title($title, $post_id)
-{
-    $post_rank = get_field('post_ranking', $post_id);
-    if (in_array(1, $post_rank)) {
-        return 'Breaking';
-    }
-    if (in_array(3, $post_rank)) {
-        return 'Leestip';
-    }
-
-    $yoast_primary_term = get_post_meta($post_id, '_yoast_wpseo_primary_regio', true) ?: '';
-    if ($yoast_primary_term) {
-        $term = get_term($yoast_primary_term, 'regio');
-        $yoast_primary_term = $term ? $term->name : '';
-    } else {
-        $terms = get_the_terms($post_id, 'regio');
-        $yoast_primary_term = $terms && !is_wp_error($terms) ? $terms[0]->name : '';
-    }
-
-    if (!empty($yoast_primary_term)) {
-        return $yoast_primary_term;
-    }
-
-    return $title;
 }
 
 function zw_webapp_call_api($post_id)
