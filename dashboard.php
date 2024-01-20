e591dd33afe10f7aa1ea668f4e045c84ad422904
rmens <r.mens@me.com>
2024-01-20 13:52:23 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -16,7 +16,7 @@ function zw_webapp_dashboard_widget_display()
     $args = array(
         'post_type' => 'post',
         'posts_per_page' => 5,
-        'meta_key' => 'push_sent', // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query -- Used in production with no issues.
+        'meta_key' => 'push_sent', // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key -- Used in production with no issues.
         'meta_value' => '1',
         'orderby' => 'date',
         'order' => 'DESC'
